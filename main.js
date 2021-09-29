97c5462199ebfa23d197e9dd7a55223beb57fd77
Mohammad Olfatee <m.olfatee@gmail.com>
2021-09-29 15:00:27 +0200

    test

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 function show() {
-    let cur = document.getElementById("currency").value;
-    console.log(cur);
-    fetch(`https://api.nomics.com/v1/currencies/ticker?key=demo-26240835858194712a4f8cc0dc635c7a&ids=${cur}&interval=1d,30d&convert=EUR`)
+    let name = document.getElementById("name").value;
+    console.log(name);
+    fetch(`https://reqres.in/api/users`)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
-            document.getElementById("content").innerHTML = `
-                 <div>
-                    <h3>ID: ${data[0].id}</h3>  
-                    <h3>Name: ${data[0].name}</h3>
-                    <h3>this currency rank is: ${data[0].rank}</h3>
-                    <img src=${data[0].logo_url} style="width: 100px;"></img > <br>
-                    <h3>this currency price is: ${data[0].price} $</h3>
-                  </div>`
-
+            console.log(data.data)
+            let persons = data.data
+            for (let item of persons) {
+                if (item.first_name === name) {
+                    console.log(item)
+                    document.getElementById("content").innerHTML = `
+                  <div>
+                     <h3>ID: ${item.id}</h3>  
+                     <h3>Name: ${item.first_name} ${item.last_name}</h3>
+                     <h3>Email address: ${item.email}</h3>
+                     <img src=${item.avatar} style="width: 100px;"></img > <br>
+                   </div>`
+                }
+            }
         })
+        .catch(error => {
+            console.log("error!")
+        })
+
 }
 
 
